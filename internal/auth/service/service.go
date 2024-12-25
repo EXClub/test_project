a149b4bbc16eebd38c348cc30f030b02bf0caafe
otie173 <otie173.email@gmail.com>
2024-12-25 09:35:12 -0500

    написал функцию для генерации хэша пароля

--- internal/auth/service/service.go
+++ internal/auth/service/service.go
@@ -1,5 +1,15 @@
 package service
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	passwordCost int = 14
+)
+
 type AuthService struct {
 }
 
@@ -7,7 +17,16 @@ func NewAuthService() *AuthService {
 	return &AuthService{}
 }
 
-func (s *AuthService) GenerateHash() {
+func (s *AuthService) GenerateHash(password []byte) ([]byte, error) {
+	if len(string(password)) <= 0 {
+		return nil, errors.New("password can ot be empty")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword(password, passwordCost)
+	if err != nil {
+		return nil, err
+	}
+	return hash, nil
 }
 
 func (s *AuthService) CompareHash() {
