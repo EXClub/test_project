0f552b9c722fa364a9a7961efcfe53c1b9e080ab
otie173 <otie173.email@gmail.com>
2024-12-25 08:48:09 -0500

    теперь ручки снова являются приватным функциями

--- api/handler.go
+++ api/handler.go
@@ -2,19 +2,19 @@ package api
 
 import "net/http"
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// логика главной
 }
 
-func AuthHandler(w http.ResponseWriter, r *http.Request) {
+func authHandler(w http.ResponseWriter, r *http.Request) {
 	// логика страницы авторизации
 }
 
-func UserPageHandler(w http.ResponseWriter, r *http.Request) {
+func userPageHandler(w http.ResponseWriter, r *http.Request) {
 	// логика страницы пользователя
 
 }
 
-func StoryHandler(w http.ResponseWriter, r *http.Request) {
+func storyHandler(w http.ResponseWriter, r *http.Request) {
 	// логика страницы с прошедшими встречами
 }
