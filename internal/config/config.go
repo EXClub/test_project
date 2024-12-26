a401dcf18434175e2d599487cd7b37bf486f477c
Arseny Gorokhov <70861765+ArsenyGorokhov@users.noreply.github.com>
2024-12-26 22:45:10 +0500

    структура

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,15 @@
 package config
 
+type Config struct {
+	IsDebug *bool `yaml:"is_debug" env-required:"true"`
+	Listen  struct {
+		Type   string `yaml:"type" env-default:"port"`
+		BindIP string `yaml:"bind_ip" env-default:"127.0.0.1"`
+		Port   string `yaml:"port" env-default:"8080"`
+	} `yaml:"listen"`
+	Storage StorageConfig `yaml:"storage"`
+}
+
 // Название говорящее)
 type StorageConfig struct {
 	Host     string `json:"host"`
